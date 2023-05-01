8764a66f6c53f1842da62c4d89a566593ca7b8a1
propakistani <39987860+propakistani@users.noreply.github.com>
2023-05-02 02:10:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const Web3 = require('web3');
+
+// Create a Web3 instance using an HTTP provider
+const web3 = new Web3('wss://mainnet.infura.io/ws/v3/8842e346de7b43d1913d438ada5ed473');
+
+// Get the balance of a wallet
+async function getBalance(walletAddress) {
+  const balance = await web3.eth.getBalance(walletAddress);
+  return web3.utils.fromWei(balance, 'ether');
+}
+
+// Call the getBalance function with a wallet address and log the result
+const walletAddress = '0x1234567890123456789012345678901234567890';
+getBalance(walletAddress).then((balance) => {
+  console.log(`Wallet balance: ${balance} ETH`);
+});
